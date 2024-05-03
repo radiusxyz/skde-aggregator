56ae2d93180f41961bfd61ca62a26c3457575fe5
woojucy <woojucy2@gmail.com>
2024-05-03 12:53:27 +0900

    Add Bench Result to README

--- benches/aggregate_with_hash.rs
+++ benches/aggregate_with_hash.rs
@@ -309,7 +309,7 @@ fn main() {
         .nresamples(10); // # of iteration
 
     let benches: Vec<Box<dyn Fn(&mut Criterion)>> = vec![Box::new(|c| {
-        bench_aggregate_with_hash::<21>("skde aggregate", c)
+        bench_aggregate_with_hash::<20>("skde aggregate", c)
     })];
 
     for bench in benches {
--- src/aggregate/mod.rs
+++ src/aggregate/mod.rs
@@ -8,7 +8,7 @@ use ff::{Field, PrimeField};
 
 use crate::big_integer::*;
 
-pub const MAX_SEQUENCER_NUMBER: usize = 27;
+pub const MAX_SEQUENCER_NUMBER: usize = 20;
 pub const BITS_LEN: usize = 2048; // n's bit length
 pub const LIMB_WIDTH: usize = 64;
 pub const LIMB_COUNT: usize = BITS_LEN / LIMB_WIDTH;
